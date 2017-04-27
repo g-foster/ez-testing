9667cd61d81bd8bd96e581d32a9e3f2a275ca17a
Gavin Foster <gavin.foster@inviqa.com>
2017-04-27 11:36:50 +0100

    .

--- ezplatform/src/Clientname/SearchBundle/Controller/DefaultController.php
+++ ezplatform/src/Clientname/SearchBundle/Controller/DefaultController.php
@@ -4,20 +4,15 @@ namespace Clientname\SearchBundle\Controller;
 
 use eZ\Bundle\EzPublishCoreBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
-use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="clientname_search_index")
+     * @Template("ClientnameSearchBundle::Default/index.html.twig")
      */
     public function indexAction()
     {
-        $response = new Response();
-        $response->headers->set('Content-Type', 'text/html');
-        $response->setContent('<html><body><h1>Hello world!</h1></body></html>');
-        $response->setStatusCode(Response::HTTP_OK);
-        return $response;
     }
 }
