49011c9a56e4063cc3df7c8cb40a894c8c5f47ee
Gavin Foster <gavin.foster@inviqa.com>
2017-04-27 11:46:52 +0100

    .

--- ezplatform/src/Clientname/SearchBundle/Controller/DefaultController.php
+++ ezplatform/src/Clientname/SearchBundle/Controller/DefaultController.php
@@ -5,14 +5,26 @@ namespace Clientname\SearchBundle\Controller;
 use eZ\Bundle\EzPublishCoreBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\DiExtraBundle\Annotation as DI;
 
 class DefaultController extends Controller
 {
+    /** @DI\Inject("inviqa.ez.articlerepository") */
+    private $articleRepository;
+
     /**
      * @Route("/", name="clientname_search_index")
      * @Template("ClientnameSearchBundle::Default/index.html.twig")
      */
     public function indexAction()
     {
+    }
+
+    /**
+     * @Template("ClientnameSearchBundle::Default/article-list.html.twig")
+     */
+    public function getArticlesAction($parentLocationId)
+    {
+        return $this->articleRepository->getArticlesByParentLocation($parentLocationId);
     }
 }
